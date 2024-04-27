5367f90030a3bd2d11cd6fd8d704d5d13908ea21
jforno <jforno@nelogica.com.br>
2024-04-27 10:52:28 -0300

    new methods

--- methods.java
+++ methods.java
@@ -2,21 +2,30 @@ public class Main {
     public static void main(String[] args) {
         int num1 = 7;
         int num2 = 5;
-
-        int total = addNumbers(num1, num2);
+        double shoeSize = 9.5;
+        String nome = "João";
+        
+        double total = addNumbers(num1, shoeSize);
         System.out.println("Sum: " + total);
 
         int difference = subtractNumbers(num1, num2);
         System.out.println("Difference: " + difference);
+        
+        printName(nome);
     }
 
     // Method to add two numbers
-    public static int addNumbers(int num1, int num2) {
+    public static double addNumbers(int num1, double num2) {
         return num1 + num2;
     }
 
     // Method to subtract two numbers
     public static int subtractNumbers(int num1, int num2) {
         return num1 - num2;
+    }
+    
+    // arrota    
+    private static void printName(String name) {
+        System.out.println(name);
     }
 }
